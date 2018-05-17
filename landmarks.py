ac21c434f64e662a27fa654754c75c605eaa7f36
Danica Reardon <danicareardon@Danicas-MacBook-Pro.local>
2018-05-17 12:52:59 +0200

    loaded landmarks by incisor

--- landmarks.py
+++ landmarks.py
@@ -1,27 +1,46 @@
 import numpy as np
+import glob
 import os
 
 class Landmarks(object):
-    """class for loading and preprocessing landmarks
-    TODO: say more abouy class
+    """place holder for pre-processing landmarks
     """
 
     def __init__(self, data):
-        """class for loading and preprocessing landmarks
+        """preprocessing landmarks
 
         Args:
             data: TODO
         """
         print("todo")
 
-    def load(directory, incisor, mirrored):
-        """loads all models for an incisor
+def load(directory, incisor, mirrored):
+    """loads all models for an incisor
 
-        Args:
-            directory: directory where the incisors are located
-                ("/_Data/Landmarks/")
-            incisor: the incisor identifier (number)
-            mirrored: boolean if mirrored or not
+    Args:
+        directory: directory where the incisors are located
+            ("/_Data/Landmarks/")
+        incisor: the incisor identifier (number)
+        mirrored: boolean if mirrored or not
 
-        Returns: a list of all landmark models
-        """
+    Returns: a list of all landmark models
+    """
+
+    files = glob.glob(directory + "original/*-" + str(incisor) + ".txt")
+    if mirrored:
+        mirrored_files = glob.glob(directory + "mirrored/*-" + str(incisor) + ".txt")
+        files += mirrored_files
+    return files
+
+if __name__ == "__main__":
+    dir = os.path.join(".", "_Data/Landmarks/")
+
+    # tests getting landmarks loaded
+    print("TEST1: loading original files")
+    files = load(dir,1,False)
+    for f in files:
+        print(str(f))
+    print("TEST2: loading original + mirrored files")
+    files = load(dir,1,True)
+    for f in files:
+        print(str(f))
