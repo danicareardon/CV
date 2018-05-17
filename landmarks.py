aca6ec356f29e6412991778a80538871f90e8e26
Danica Reardon <danicareardon@Danicas-MacBook-Pro.local>
2018-05-17 13:43:03 +0200

    reads the landmarks

--- landmarks.py
+++ landmarks.py
@@ -3,19 +3,35 @@ import glob
 import os
 
 class Landmarks(object):
-    """place holder for pre-processing landmarks
+    """class for all landmarks of one incisor
     """
 
     def __init__(self, data):
-        """preprocessing landmarks
+        """new set of landmarks
 
         Args:
-            data: TODO
+            data (str || ?): The filepath of the landmark file
         """
-        print("todo")
+        if data:
+            if isinstance(data, str):
+                _read_landmarks(data)
 
-def load(directory, incisor, mirrored):
-    """loads all models for an incisor
+    def _read_landmarks(self, file):
+        """reads the landmarks from a file
+
+        Args:
+            file: path to landmark file
+        """
+        points = []
+        #https://stackoverflow.com/questions/1657299/how-do-i-read-two-lines-from-a-file-at-a-time-using-python
+        lines = open(file).readlines()
+        for x, y in zip(lines[0::2], lines[1::2]):
+            points.append(np.array([float(x),float(y)]))
+        self.points = np.array(points)
+
+
+def load_landmarks(directory, incisor, mirrored):
+    """loads all models for one specific incisor
 
     Args:
         directory: directory where the incisors are located
@@ -23,24 +39,26 @@ def load(directory, incisor, mirrored):
         incisor: the incisor identifier (number)
         mirrored: boolean if mirrored or not
 
-    Returns: a list of all landmark models
+    Returns: a Landmarks object for one incisor
     """
-
+    landmarks = []
     files = glob.glob(directory + "original/*-" + str(incisor) + ".txt")
     if mirrored:
         mirrored_files = glob.glob(directory + "mirrored/*-" + str(incisor) + ".txt")
         files += mirrored_files
-    return files
+    for file in files:
+        landmarks.append(Landmarks(file))
+    return landmarks
 
 if __name__ == "__main__":
     dir = os.path.join(".", "_Data/Landmarks/")
 
     # tests getting landmarks loaded
-    print("TEST1: loading original files")
-    files = load(dir,1,False)
-    for f in files:
-        print(str(f))
-    print("TEST2: loading original + mirrored files")
-    files = load(dir,1,True)
-    for f in files:
-        print(str(f))
+    # print("TEST1: loading original files")
+    # files = load(dir,1,False)
+    # for f in files:
+    #     print(str(f))
+    # print("TEST2: loading original + mirrored files")
+    # files = load(dir,1,True)
+    # for f in files:
+    #     print(str(f))
