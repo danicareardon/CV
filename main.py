a11e2f99070a71f0340b6c04236e763bb1ad8666
Danica Reardon <danicareardon@Danicas-MacBook-Pro.local>
2018-05-17 14:11:09 +0200

    main function

---
+++ main.py
@@ -0,0 +1,33 @@
+"""Incisor Segmentation main function
+
+Author: Danica Reardon
+Student Number: r0604915
+
+Author: Danica Reardon
+Student Number: r0604915
+
+Python 3.5
+"""
+
+from landmarks import load_landmarks
+
+def main():
+    directory = os.path.join(".", "/_Data/Landmarks/")
+    incisor_segmentations = []
+
+    for incisor in range(1,9):
+        # 1.1 load landmarks
+        landmarks = load_landmarks(directory, incisor, mirrored=True)
+
+        # 1.2 process landmarks (Procrustes Analysis?)
+
+        # 1.3
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
