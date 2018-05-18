51aa318f202ee28bb41f216b7f5c030981ff3b08
Akash Madhusudan <amverma48@gmail.com>
2018-05-18 19:31:41 +0200

    Added class for PCA

--- main.py
+++ main.py
@@ -12,24 +12,21 @@ Python 3.5
 import os
 from landmarks import load_landmarks, Landmarks
 from procustes_analysis import procrustes
+from shape_variations import PCA
+
 
 def main():
     directory = os.path.join(".", "_Data/Landmarks/")
 
-    for incisor in range(1,9):
+    for incisor in range(1, 9):
         # 1.1 load landmarks
         landmarks = load_landmarks(directory, incisor, mirrored=True)
-
 
         # 1.2 process landmarks (Procrustes Analysis?)
         procrustes(landmarks)
 
         # 1.3
-
-
-
-
-
+        PCA(landmarks)
 
 if __name__ == "__main__":
     main()
